39e06349e9086c1e4e6d8280df56bd779b4747db
Jesus <Jesus@LAPTOP-9O0FI901>
2021-04-20 19:41:22 -0500

    Commit1

---
+++ src/codigo1/Codigo1.java
@@ -0,0 +1,20 @@
+
+package codigo1;
+
+
+public class Codigo1 {
+
+    
+    public static void main(String[] args) {
+        String sMesaje = "Lenguaje de Programación I, Grupo A";
+        String sError = "Mensaje Contrario";
+        boolean linea = false;
+        
+        if (linea){
+            System.out.println(sMesaje);
+        }else{
+            System.out.println(sError);
+        }
+    }
+    
+}
